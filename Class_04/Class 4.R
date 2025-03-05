b8204122da2bd92ca4ce4fd8868544162cc53a7c
Safiya <ssayd@ucsd.edu>
2025-03-04 19:23:35 -0800

    Add class_04

---
+++ Class_04/Class 4.R
@@ -0,0 +1,3 @@
+x <- 1:50
+plot(x, sin(x))
+plot(x, sin(x), col="blue", type="l", lwd=3)
---
+++ Class_04/PDF rstudio.R
@@ -0,0 +1,10 @@
+#' ---
+#' title: "Class 4: Intro ro R"
+#' author: "Safiya Sayd (A18027139)"
+#' date: "May 3rd, 2014"
+#' ---
+
+
+x <- 1:50
+plot(x, sin(x))
+plot(x, sin(x), col="blue", type="l", lwd=3)
